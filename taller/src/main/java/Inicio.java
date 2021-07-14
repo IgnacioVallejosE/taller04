f24f753817624d006794fe71942e91cdcdc5bb5c
Nacho <i.vallejos01@ufromail.cl>
2021-07-14 10:08:00 -0400

    Avance

--- taller/src/main/java/Inicio.java
+++ taller/src/main/java/Inicio.java
@@ -5,4 +5,13 @@ public class Inicio extends JFrame {
     private JPanel jpanel;
     private JComboBox comboBox1;
 
+    public Inicio() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public JComboBox getComboBox1() {
+
+        return comboBox1;
+    }
 }
